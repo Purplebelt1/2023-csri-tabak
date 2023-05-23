9b13b87771dec78b7746e51e248680ff20e0d5dc
Purplebelt1 <wdeb2013@gmail.com>
2023-05-23 08:55:37 -0500

    Added black and white image enhance

---
+++ code/nitrateFilm.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageEnhance
+
+image = Image.open("./images/barn_house.jpg")
+
+color_enhancer = ImageEnhance.Color(image)
+
+enhanced_image = color_enhancer.enhance(0)
+
+enhanced_image.show()
