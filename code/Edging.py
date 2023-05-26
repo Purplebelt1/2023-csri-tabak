8ed62b91ed0b40eecd561fcf77dabf4e39897cbf
ringoldj <justinringold@me.com>
2023-05-26 10:40:59 -0500

    cleaned up edging and commented

--- code/Edging.py
+++ code/Edging.py
@@ -1,14 +1,25 @@
 from PIL import Image, ImageFilter, ImageChops
 
-calm_image = Image.open("images/calm_water.JPG")
+#gets edges plus some by using gaussian blur and merging images
+def edging(img):
+    im = img.filter(ImageFilter.GaussianBlur(200))
+    im2 = img.filter(ImageFilter.GaussianBlur(1))
+    return ImageChops.subtract(image1 = im, image2 = im2)
 
-im = calm_image.filter(ImageFilter.GaussianBlur(200))
-im2 = calm_image.filter(ImageFilter.GaussianBlur(1))
-im3 = ImageChops.subtract(image1 = im, image2 = im2)
+#makes half the image a negative
+def negative_half(img):
+    box = (0,0, img.size[0] // 2, img.size[1])
+    region = img.crop(box)
+    img = img.point(lambda p: 255 - p)
+    img.paste(region, box)
+    return img
 
-box = (0,0, 2016, 3024)
-region = im3.crop(box)
+#opens image and applies changes to it
+def main():
+    calm_image = Image.open("images/calm_water.JPG")
+    im = edging(calm_image)
+    im2 = negative_half(im)
+    im2.show()
 
-im3 = im3.point(lambda p: 255 - p)
-im3.paste(region, box)
-im3.show()
+if __name__ == '__main__':
+    main()
