23f153547c07fa47d838e6eb948490b8ded916f8
ringoldj <justinringold@me.com>
2023-05-26 11:38:13 -0500

    cleaned up code and added comments to point

--- code/point.py
+++ code/point.py
@@ -1,52 +1,22 @@
 from PIL import Image, ImageFile, ImageOps, ImageEnhance, ImageChops, ImageFilter
-ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-#def pixelProcRed(intensity):
-#
-#    return 0
-#
-#def pixelProcBlue(intensity):
-#
-#    return (intensity * 2) % 255
-#
-#def pixelProcGreen(intensity):
-#
-#    return intensity / 2
+def main():
+    #allows image to show
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-im2 = Image.open("images/pointed.JPG")
-im3 = Image.open("images/faded.JPG")
-im4 = Image.open("/home/justin/Documents/2023-csri-tabak/images/water _view.JPG")
+    im2 = Image.open("images/pointed.JPG")
+    im3 = Image.open("images/faded.JPG")
 
+    #This all is just semi-random changes made until the image looked nice
+    im = im2.point(lambda p: 255 - p)
+    im = im.point(lambda p: p-100 if(p>100) else (p+120))
+    im = im.filter(ImageFilter.GaussianBlur(200))
+    im = ImageChops.subtract(image1 = im, image2 = im2)
+    enhanced_im = ImageEnhance.Brightness(im)
+    im = enhanced_im.enhance(2)
+    im = ImageChops.subtract(image1 = im3, image2 = im)
 
-im = im2.point(lambda p: 255 - p)
-im = im.point(lambda p: p-100 if(p>100) else (p+120))
-im = im.filter(ImageFilter.GaussianBlur(200))
-im = ImageChops.subtract(image1 = im, image2 = im2)
-#im = im.point(lambda p: 255 - p)
-enhanced_im = ImageEnhance.Brightness(im)
-im = enhanced_im.enhance(2)
-#m = im.convert("L")
-im = ImageChops.subtract(image1 = im3, image2 = im)
+    im.show()
 
-
-#multiBands = im.split()
-#
-#redBand = multiBands[0].point(pixelProcRed)
-#
-#greenBand = multiBands[1].point(pixelProcGreen)
-#
-#blueBand = multiBands[2].point(pixelProcBlue)
-#
-#im = Image.merge("RGB", (redBand, greenBand, blueBand))
-
-#enhanced_im = ImageEnhance.Brightness(im)
-#im = enhanced_im.enhance(2)
-
-
-#im4 = im4.point(lambda p: 255 - p)
-#im = ImageChops.subtract(image1 = im4, image2 = im)
-
-#im = im.point(lambda p: 255 - p)
-
-im.show()
-#im2.show()
+if __name__ == '__main__':
+    main()
