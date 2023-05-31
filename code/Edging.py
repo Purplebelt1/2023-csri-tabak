382e9e0d94e06635421290d8a56d1eb4fcca730d
ringoldj <justinringold@me.com>
2023-05-31 15:26:51 -0500

    adjusted edging and added checkers methods added checkers to results

--- code/Edging.py
+++ code/Edging.py
@@ -14,12 +14,46 @@ def negative_half(img):
     img.paste(region, box)
     return img
 
+#makes two quaters of the image a negative
+def negative_checker(img):
+    box = (0,0, img.size[0] // 2, img.size[1] // 2)
+    region = img.crop(box)
+    box2 = (img.size[0] // 2, img.size[1] // 2, img.size[0], img.size[1])
+    region2 = img.crop(box2)
+    img = img.point(lambda p: 255 - p)
+    img.paste(region, box)
+    img.paste(region2, box2)
+    return img
+
 #opens image and applies changes to it
+def checker_plus(img):
+    im = img.copy()
+    boxul = (0,0, im.size[0] // 2, im.size[1] // 2)
+    boxbl = (0, im.size[1] // 2, im.size[0] // 2, im.size[1])
+    boxur = (im.size[0] // 2, 0, im.size[0], im.size[1] // 2)
+    boxbr = (im.size[0] // 2, im.size[1] // 2, im.size[0], im.size[1])
+    regionul = im.crop(boxul)
+    regionbl = im.crop(boxbl)
+    regionur = im.crop(boxur)
+    regionbr = im.crop(boxbr)
+    nul = negative_checker(regionul)
+    nbl = negative_checker(regionbl)
+    nur = negative_checker(regionur)
+    nbr = negative_checker(regionbr)
+    im.paste(nul, boxul)
+    im.paste(nbl, boxbl)
+    im.paste(nur, boxbr)
+    im.paste(nbr, boxur)
+    return im
+
+
 def main():
     calm_image = Image.open("images/calm_water.JPG")
     im = edging(calm_image)
-    im2 = negative_half(im)
+    im2 = checker_plus(calm_image)
     im2.show()
+    #im2 = negative_half(im)
+    #im3 = negative_checker(im)
 
 if __name__ == '__main__':
     main()
