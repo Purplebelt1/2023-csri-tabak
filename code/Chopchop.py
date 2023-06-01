443ee60d6db9337812723adf2492fd1afac9fd74
ringoldj <justinringold@me.com>
2023-06-01 09:24:50 -0500

    added more comments

--- code/Chopchop.py
+++ code/Chopchop.py
@@ -1,21 +1,31 @@
-from PIL import Image, ImageChops, ImageFilter
+from PIL import Image, ImageChops
 import math
 
 def shiftSubtract(im, offset):
+    #Takes image im and creates a offset version.
+    #The new image and im are subtracted and then
+    #cropped to remove the parts that looped around
+    #to the other side of the image.
+
+    #finds smallest integer ratio of width and height
     div = math.gcd(im.size[0], im.size[1])
     xdim = im.size[0] // div
     ydim = im.size[1] // div
+    #multiplies ratio by offset so offset is applied 
+    #according to the width and height ratio
     xoff = xdim * offset
     yoff = ydim * offset
+    #creates offset image and subtracts with original
     im2 = ImageChops.offset(im, xoff, yoff)
     im3 = ImageChops.difference(im, im2)
+    #removes looped parts of image
     im3 = im3.crop((xoff, yoff, im3.size[0], im3.size[1]))
     return im3
 
 
 def main():
+    #tests shiftSubtract
     im = Image.open("images/calm_water.JPG")
-    im2 = Image.open("images/pointed.JPG")
     im3 = shiftSubtract(im, 90)
     im3 = im3.crop((0,0,im3.size[0], (im3.size[1] * 7)//10))
     im3.show()
--- code/Edging.py
+++ code/Edging.py
@@ -1,8 +1,9 @@
 from PIL import Image, ImageFilter, ImageChops
 
 #gets edges plus some by using gaussian blur and merging images
-def edging(img):
-    im = img.filter(ImageFilter.GaussianBlur(200))
+#blur determiness the amount of blur used
+def edging(img, blur):
+    im = img.filter(ImageFilter.GaussianBlur(blur))
     im2 = img.filter(ImageFilter.GaussianBlur(1))
     return ImageChops.subtract(image1 = im, image2 = im2)
 
@@ -25,21 +26,27 @@ def negative_checker(img):
     img.paste(region2, box2)
     return img
 
-#opens image and applies changes to it
+#Takes img and calls negative_checker on its
+#four corners. The bottom right and upper right
+#corners are swapped.
 def checker_plus(img):
     im = img.copy()
+    #defines four corners of image
     boxul = (0,0, im.size[0] // 2, im.size[1] // 2)
     boxbl = (0, im.size[1] // 2, im.size[0] // 2, im.size[1])
     boxur = (im.size[0] // 2, 0, im.size[0], im.size[1] // 2)
     boxbr = (im.size[0] // 2, im.size[1] // 2, im.size[0], im.size[1])
+    #takes corners out
     regionul = im.crop(boxul)
     regionbl = im.crop(boxbl)
     regionur = im.crop(boxur)
     regionbr = im.crop(boxbr)
+    #applies checker like pattern to each corner
     nul = negative_checker(regionul)
     nbl = negative_checker(regionbl)
     nur = negative_checker(regionur)
     nbr = negative_checker(regionbr)
+    #puts corners back slightly shifted
     im.paste(nul, boxul)
     im.paste(nbl, boxbl)
     im.paste(nur, boxbr)
@@ -47,13 +54,11 @@ def checker_plus(img):
     return im
 
 
+#tests checker_plus
 def main():
     calm_image = Image.open("images/calm_water.JPG")
-    im = edging(calm_image)
     im2 = checker_plus(calm_image)
     im2.show()
-    #im2 = negative_half(im)
-    #im3 = negative_checker(im)
-
+    
 if __name__ == '__main__':
     main()
