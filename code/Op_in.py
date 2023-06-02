a212dea0df9cea3562fb07828b1fbe8c26000b25
ringoldj <justinringold@me.com>
2023-06-02 14:13:27 -0500

    added images

--- code/Op_in.py
+++ code/Op_in.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageOps, ImageFile, ImageEnhance
+from PIL import Image, ImageOps, ImageFile, ImageEnhance, ImageFilter
 import Chopchop
 import Edging
 
@@ -16,20 +16,27 @@ def poster(im, chanels):
 
 def main():
     ImageFile.LOAD_TRUNCATED_IMAGES = True
-    im = Image.open("images/water _view.JPG")
+    im = Image.open("images/embossed.jpg")
     #im2 = contrastEnhance(im, 30, 3)
     im3 = im.convert("L")
     im3 = ImageOps.equalize(im3)
-    im4 = ImageOps.colorize(im3, (153,75,0), "Purple", "Orange")#(1, 92, 31), "Purple", (88, 149, 219)) #(153, 75, 0) - dark orange
+    im4 = ImageOps.colorize(im3, "darkblue", "Darkgoldenrod")#"Blue", "Yellow", "Green")
+    # - for purple sky -(153,75,0), "Purple", "Orange")
+    #(1, 92, 31), "Purple", (88, 149, 219)) #(153, 75, 0) - dark orange
     im5 = Edging.edging(im4, 2)
     im6 = im5.convert("1")
     im6 = im6.convert("L")
     im7 = ImageOps.colorize(im6, "black", "white", "black")
-    im8 = poster(im, 1)
+    im8 = poster(im, 2)
+    #im4 = im4.filter(ImageFilter.GaussianBlur(2))
+    #im4 = ImageEnhance.Sharpness(im4)
+    #im4 = im4.enhance(2)
     #im4.save("base.jpg")
     #im7.save("outline.jpg")
     #im6.show()
-    im8.show()
+    #im8.save("to_emboss.jpg")
+    #im4.save("post_butchering.jpg")
+    im4.show()
 
 if __name__ == '__main__':
     main()
