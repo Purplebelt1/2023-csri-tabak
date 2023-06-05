111cad5d0b5b843e0b12320d73af1a00304bb4bb
ringoldj <justinringold@me.com>
2023-06-05 16:04:48 -0500

    blame Will

---
+++ code/openCVFun.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+import Result
+import numpy as np
+image = np.zeros((1000,1000,3), np.uint8)
+font = cv.FONT_HERSHEY_SIMPLEX
+org = (100, 500)
+fontScale = 3
+color = (255, 0, 255)
+thickness = 10
+image = cv.putText(image, 'WHYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY!', org, font, 
+                   fontScale, color, thickness, cv.LINE_AA)
+Result.singleWindow(image)
