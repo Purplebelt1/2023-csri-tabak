a7325683ecf06992a043d351536ef6015d39689a
ringoldj <justinringold@me.com>
2023-06-06 10:27:01 -0500

    starting opencv

--- code/seev.py
+++ code/seev.py
@@ -1,3 +1,15 @@
 import cv2 as cv
 import math
 import numpy as np
+import Result
+
+def trig(im):
+    
+
+def main():
+    im = cv.imread('./images/moss.jpg')
+    hcl = cv.cvtColor(im, cv.COLOR_BGR2LAB)
+    Result.singleWindow(hcl)
+
+if __name__ == '__main__':
+    main()
