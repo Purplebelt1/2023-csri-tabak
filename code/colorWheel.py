1790f0d763ff038a8164e631ce32328cee4f512e
ringoldj <justinringold@me.com>
2023-07-07 15:46:01 -0500

    added more comments to colorWheel

--- code/colorWheel.py
+++ code/colorWheel.py
@@ -1,10 +1,6 @@
 import cv2 as cv
 import numpy as np
 import Result
-import Filters
-import Posterization
-
-
 
 def monochromatic(img, hue):
     #changes all hues in img to to hue and returns new image
@@ -193,113 +189,144 @@ def analogous_shadows(img, hue, lbound = 20, hbound = 80):
 
 
 def tetradic(img, hue):
+    #changes all hues in img to to hue or its tetradic colors and returns new image
+    #copies img, converts it to hsv, and splits the channels
     im = img.copy()
     hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
     h,s,v = cv.split(hsv)
+    #prevents hue from being outside applicable range and calculates tetradic hue
     hue = hue % 180
     sec = (hue + 45) % 180
     ter = (hue + 90) % 180
     qua = (hue + 135) % 180
+    #makes new hue channel where hues are changed to hue, sec, ter, or qua depending
+    #on which is closer in value
     new_hue = np.where(h < 45, hue, 181)
     new_sec = np.where(np.logical_and(h < 90, new_hue == 181), sec, new_hue)
     new_ter = np.where(np.logical_and(h < 135, new_sec == 181), ter, new_sec)
     new_qua = np.where(new_ter == 181, qua, new_ter).astype('uint8')
+    #merges new hue channel with other channels and converts back to bgr
     ret_im = cv.merge((new_qua,s,v))
     ret_im = cv.cvtColor(ret_im, cv.COLOR_HSV2BGR)
     return ret_im    
 
 def tetradic_shadows(img, hue, lbound = 25, mbound = 50, hbound = 75):
+    #changes all hues in img to to hue or its tetradic colors and returns new image
+    #lbound, mbound, and hbound determine the value percentiles that act
+    #as boundaries between hues
+    #copies img, converts it to hsv, and splits the channels
     im = img.copy()
     hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
     h,s,v = cv.split(hsv)
+    #prevents hue from being outside applicable range and calculates tetradic hue
     hue = hue % 180
     sec = (hue + 45) % 180
     ter = (hue + 90) % 180
     qua = (hue + 135) % 180
+    #finds value matching the percentiles of lbound, mbound, and hbound
     low = np.percentile(v, lbound)
     mid = np.percentile(v, mbound)
     high = np.percentile(v, hbound)
+    #makes new hue channel where hues are changed to hue, sec, ter, or qua depending
+    #on the values at the location of the hue
     new_hue = np.where(v < high, hue, qua)
     new_ter = np.where(v < mid, ter, new_hue)
     new_sec = np.where(v < low, sec, new_ter).astype('uint8')
+    #merges new hue channel with other channels and converts back to bgr
     ret_im = cv.merge((new_sec,s,v))
     ret_im = cv.cvtColor(ret_im, cv.COLOR_HSV2BGR)
     return ret_im    
 
 def value_split(img, split, round_down = True):
+    #limits the number of possible values in img based on split and returns new image
+    #round_down determines whether the values will be rounded down to the nearest
+    #applicable value or rounded up
+    #copies img, converts it to hsv, and splits the channels
     im = img.copy()
     hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
     h,s,v = cv.split(hsv)
+    #determines a divisor to use for spliting values
     point = 255 // (split - 1)
-    print(v)
+    #creates new value channel by floor dividing by divsor and multipling by divisor
     if round_down:
         new_val = np.multiply(np.floor(np.divide(v, point)), point).astype('uint8')
     else:
         new_val = np.multiply(np.minimum((split - 1),np.ceil(np.divide(v, point))), point).astype('uint8')
-    print(new_val)
+    #merges new value channel with other channels and converts back to bgr
     ret_im = cv.merge((h,s,new_val))
     ret_im = cv.cvtColor(ret_im, cv.COLOR_HSV2BGR)
     return ret_im    
 
-def complementary_shadows(img, hue, value_cutoff = -1):
+def complementary_shadows(img, hue, mbound = 50):
+    #changes all hues in img to to hue or its complementary color and returns new image
+    #mbound determine the value percentile that act as a boundary between hues
+    #copies img, converts it to hsv, and splits the channels
     im = img.copy()
     hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
     h,s,v = cv.split(hsv)
+    #prevents hue from being outside applicable range and calculates complementary hue
     hue = hue % 180
     opis = (hue + 90) % 180
-    if value_cutoff != -1:
-        median = value_cutoff
-    else:
-        median = np.median(v)
-    new_hue = np.where(v < median, opis, hue).astype('uint8')
+    #finds value matching the percentile of mbound
+    mid = np.percentile(v, mbound)
+    #makes new hue channel where hues are changed to hue or opis depending
+    #on the values at the location of the hue 
+    new_hue = np.where(v < mid, opis, hue).astype('uint8')
+    #merges new hue channel with other channels and converts back to bgr
     ret_im = cv.merge((new_hue, s,v))
     ret_im = cv.cvtColor(ret_im, cv.COLOR_HSV2BGR)
     return ret_im
 
-def swap(img, val = 250, sat = 5):
-    im = img.copy()
-    hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
-    h,s,v = cv.split(hsv)
-    darken = np.where(np.logical_and(v > val, s < sat), 0, v).astype('uint8')
-    ret_im = cv.merge((h,s,darken))
-    ret_im = cv.cvtColor(ret_im, cv.COLOR_HSV2BGR)
-    return ret_im
-
 def hue_shift(img, hue = 90):
+    #shifts all hues in img by hue and returns new image
+    #creates copy of img, converts it hsv, and splits the channels
     im = img.copy()
-    hue = hue % 180
     hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
     h,s,v = cv.split(hsv)
-    #print(h[2200][3000])
-    #new_hue = np.mod(np.add(h, hue), 180).astype('uint8')
-    #test = np.add(h, hue).astype('uint8')
+    #adjusts hue to be applicable range and
+    hue = hue % 180
+    #calculates new hue channel
+    #when adding hue would result in a nonappliacble value
+    #the hue values are subtracted from alt_hue instead for an applicable value
     alt_hue = 180 - hue
     new_hue = np.where(alt_hue <= h, h - alt_hue, h + hue).astype('uint8')
-    #print(new_hue[2200][3000])
+    #merges new hue channel with other channels and converts back to bgr
     ret_im = cv.merge((new_hue,s,v))
     ret_im = cv.cvtColor(ret_im, cv.COLOR_HSV2BGR)
     return ret_im
 
 def saturation_drain(img, percent_sat):
+    #lowers the saturation in img to the percentage percent_sat
+    #of the original saturation and returns a new image
+    #creates copy of img
     im = img.copy()
+    #adjusts percent_sat to applicable range
     if percent_sat > 100:
         percent_sat = 100
     elif percent_sat < 0:
         percent_sat = 0
     percent_sat = percent_sat / 100
+    #converts img copy to hsv and splits its channels
     hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
     h,s,v = cv.split(hsv)
+    #makes new saturation channel
     new_sat = (s * percent_sat).astype('uint8')
+    #merges new saturation channel with other channels and converts back to bgr
     ret_im = cv.merge((h,new_sat,v))
     ret_im = cv.cvtColor(ret_im, cv.COLOR_HSV2BGR)
     return ret_im
 
 def set_saturation(img, sat):
+    #changes all values in img to val and returns new image
+    #copies img, converts it to hsv, and makes sure sat is within aplicable range
     im = img.copy()
     sat = sat % 256
     hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
+    #splits image into hue, saturation, and value channels
     h,s,v = cv.split(hsv)
+    #makes new saturation channel
     new_sat = (np.ones(s.shape) * sat).astype('uint8')
+    #merges new saturation channel with other channels and converts back to bgr
     ret_im = cv.merge((h,new_sat,v))
     ret_im = cv.cvtColor(ret_im, cv.COLOR_HSV2BGR)
     return ret_im
@@ -321,48 +348,15 @@ def set_value(img, val):
 
 
 def main():
-    #3306 is cute
-    #im = cv.imread('images/puppies/IMG_3304.jpeg')#('images/puppies/IMG_2876.jpeg')
-    #im = cv.imread('images/italy/IMG_5845.jpeg')
-    #im = cv.imread('images/skyline.jpg')
-    #im = cv.imread('images/italy/IMG_5794.jpeg')
-    #im = cv.imread('images/other_stuff/IMG_2224.jpeg')#4056.jpeg')#2541.jpeg')
-    #im = cv.imread('images/puppies/IMG_3306.jpeg')#329.jpeg')
-    #org = monochromatic(im, 70)
-    #new_im = analogous(im, 75)
-    #im2 = shadows(im, 70)
-    #cv.imwrite('analogous.jpg', new_im)
-    #cv.imwrite('shadows.jpg', im2)
-    #im2 = value_split(im, 3)
-    #im = cv.imread('images/other_stuff/IMG_2107.jpeg')#3611.jpeg')
-    #im = cv.imread('images/new/IMG_3691.jpeg')
-    #im = cv.imread('images/new/IMG_3693.jpeg')
-    #im = cv.imread('images/new/IMG_3728.jpeg')
     im = cv.imread('images/justin/IMG_3731.jpeg')
 
-    #im2 = split_complimentary_shadows(im, 90, 20, 80)
-    #im3 = analogous_shadows(im, 70)
     im2 = split_complimentary_shadows(im, 134, 20, 80)
-    #im2 = tetradic(im, 134)
     im3 = tetradic_shadows(im, 179, 25, 50, 75)
-    #im2 = monochromatic(im, 0)
-
-    #im2 = set_saturation(im, 55)
-    #im2 = set_value(im2, 255)
-    
-    #im = cv.imread('images/italy/308d245f-3b40-46aa-adf7-33c0250f4470.jpeg')
-
-    #im2 = hue_shift(im, 80)
-    
-    #im2 = split_complimentary_shadows(im, 100, 20, 60)
     
     #cv.imwrite('undecided.jpg', im2)
     #cv.imwrite('undecided2.jpg', im3)
 
-
-   
     Result.singleWindow([im2, im3], imDim = im.shape[:2])#, dtype= 's')
-    #Result.singleWindow(im)
-
+    
 if __name__ == '__main__':
     main()
